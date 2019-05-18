079f46401de3f14335f9dfb28a0a91c5d5c33e5c
William Robertson <c.william.robertson@gmail.com>
2019-05-18 17:21:58 +0200

    checks username and saves to DB

--- server/routes/userCredentials.ts
+++ server/routes/userCredentials.ts
@@ -1,9 +1,23 @@
-import {Request, Response, Router} from 'express'
+import { Request, Response, Router } from 'express'
+import { createUser } from '../userModel'
 
 const router: Router = Router()
 
-router.post('/', (req: Request, res: Response) => {
-  res.status(200)
+router.post('/', async (req: Request, res: Response) => {
+  const postBody: IPostBody = req.body
+
+  if (checkUsernameRegexp(postBody.username)) {
+    const user = await createUser(postBody)
+    res.json({ message: 'success!' })
+  } else {
+    res.sendStatus(500)
+  }
 })
+
+function checkUsernameRegexp(username: string) {
+  const regexp = new RegExp('[A-Ö|a-ö]{2}[1-9]{3}[A-Ö|a-ö]{2}')
+
+  return regexp.test(username)
+}
 
 export const userCredentials: Router = router
